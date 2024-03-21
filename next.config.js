e82cb1d144e0cd98156317746beb11e7d0d3931e
Daniel Sousa <daniel.sousa@elo.com.br>
2024-03-20 21:02:41 -0300

    testing with fallback instead

--- next.config.js
+++ next.config.js
@@ -1,51 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      // MF que tem todas as rotas autenticadas
-      //
-      // Obs.: Cada deve configurar o próprio middleware com next-auth
-      // {
-      //   source: "/auth-only",
-      //   destination: `${MF_AUTH_ONLY}/auth-only`,
-      // },
-      // {
-      //   source: "/auth-only/:path",
-      //   destination: `${MF_AUTH_ONLY}/auth-only/:path*`,
-      // },
-      //
-      // -------------------------------------------------------------------
-      //
-      // MF que tem rotas públicas e autenticadas
-      // {
-      //   source: "/mixed",
-      //   destination: `${MF_MIXED}/mixed`,
-      // },
-      // {
-      //   source: "/mixed/:path",
-      //   destination: `${MF_MIXED}/mixed/:path*`,
-      // },
-      // MF que tem todas as rotas de acesso públicos
-      //
-      // -------------------------------------------------------------------
-      //
-      // Obs.: O código abaixo consegue manipular todas as rotas base da URL
-      // logo para que as demais rotas como /mixed e /auth-only funcionem
-      // esse objeto SEMPRE deve ser o último da lista, caso contrário
-      // nenhum outro MF vai conseguir se conectar
-      {
-        source: "/", // grupo de controle
-        destination: "https://gentle-grass-06d7e0a10.5.azurestaticapps.net", // MF responsável
-      },
-      // O objeto abaixo pode levar a comportamentos inesperados
-      // devido a possibilidade de conflito com as demais rotas
-      // caso tenham os mesmos nomes, para evitar isso, esse deve ser o último
-      // item dos rewrites, assim conflitos de rotas iram priorizar as demais
-      {
-        source: "/:path",
-        destination: "https://gentle-grass-06d7e0a10.5.azurestaticapps.net/:path*",
-      },
-    ]
+    return {
+      fallback: [
+        // These rewrites are checked after both pages/public files
+        // and dynamic routes are checked
+        {
+          source: "/", // grupo de controle
+          destination: "https://gentle-grass-06d7e0a10.5.azurestaticapps.net", // MF responsável
+          // destination: "http://localhost:3001", // MF responsável
+        },
+        {
+          source: "/:path",
+          destination: "https://gentle-grass-06d7e0a10.5.azurestaticapps.net/:path*",
+          // destination: "http://localhost:3001/:path*",
+        },
+      ],
+    }
   },
   async redirects() {
     return [
